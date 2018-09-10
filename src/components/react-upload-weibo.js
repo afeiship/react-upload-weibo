0b125749fbe016d5465f8a97fb70722a82b605c8
afeiship <1290657123@qq.com>
2018-09-10 18:09:29 +0800

    issue: opmtize code

--- src/components/react-upload-weibo.js
+++ src/components/react-upload-weibo.js
@@ -5,12 +5,7 @@ import classNames from 'classnames';
 import noop from 'noop';
 import objectAssign from 'object-assign';
 import ReactUpload from 'react-upload';
-import NxFileUpload from 'next-file-upload';
-import NxWeiboToPics from 'next-weibo-to-pics';
-
-
-const WEIBO_API = '/weibo_api/interface/pic_upload.php';
-const WEIBO_IMG = 'https://ws2.sinaimg.cn';
+import NxWeiboOss from 'next-weibo-oss';
 
 // Refused to set unsafe header "Cookie"
 // DO NOT USE xhr.setHeader to set cookie
@@ -32,42 +27,20 @@ export default class extends Component {
   };
   /*===properties end===*/
 
-  initialToken() {
-    const { token } = this.props;
-    let subCookie = token;
-    if (typeof token !== 'string') {
-      subCookie = token.find(item => {
-        return item.indexOf('SUB=') > -1;
-      });
-    }
-    document.cookie = subCookie.split(';')[0];
+  constructor(inProps) {
+    super(inProps);
+    this._oss = new NxWeiboOss(inProps.token);
   }
 
   _onChange = inEvent => {
     const { onChange } = this.props;
     const { value } = inEvent.target;
-    const request = {};
-    //SET COOKIE BEFORE:
-    this.initialToken();
-    nx.forEach(value, (file, index) => {
-      request[`pic${index + 1}`] = file;
-    });
 
-    NxFileUpload(WEIBO_API, request).then(response => {
-      const _response = JSON.parse(response.split('\n')[2]);
-      const data = _response.data;
-      if (data.count > 0) {
-        const pics = NxWeiboToPics(data.pics);
-        const value = pics.map(item => {
-          item.type = item.pid.charAt(21) === 'g' ? 'gif' : 'jpg';
-          item.url = `${WEIBO_IMG}/large/${item.pid}.${item.type}`;
-          return item;
-        });
-        onChange({ target: { value } });
-      } else {
-        console.error('[UPLOAD FAILED]:', response, _response);
-      }
-    });
+    this._oss.uploads(value).then(response => {
+      onChange({ target: { value: response } });
+    }, error => {
+      console.error('[UPLOAD FAILED]:', error);
+    })
   };
 
   render() {
